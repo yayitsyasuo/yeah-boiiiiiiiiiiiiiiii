db2a4b401fb5245bb6f5977cec103597cebdacfc
Marcin Walaszczyk <01150083@pw.edu.pl>
2019-12-18 15:11:49 +0100

    what if its

--- Full Body Orgasm Snake/Engine/Frame.cpp
+++ Full Body Orgasm Snake/Engine/Frame.cpp
@@ -8,18 +8,29 @@ Frame::Frame(Graphics& gfx)
 
 void Frame::DrawAlles()
 {
+	//left top-bottom tri
 	Location LTL = Location ( Gap, Gap ); // left top left
 	TriLTL(LTL, LL);
-	LTL =  Location (Gap+ TriWidth, Gap+TriWidth); // left top right
+	Location LBL = Location(Gap, gfx.ScreenHeight - Gap - TriWidth); // left bottom left
+	TriLDL(LBL, LL);
+
+	//left top-bottom rect
+	Location RectTopLeft = LTL + Location(0, TriWidth);
+	Location RectBottomLeft = LBL + Location(TriWidth, 0);
+	Rect(RectTopLeft, RectBottomLeft, LL); //WRONG DIMENSION :>
+
+	LTL =  Location (Gap+TriWidth, Gap + TriWidth); // left top right
 	TriLTL(LTL, LR);
-	LTL = Location(Gap, gfx.ScreenHeight - Gap - TriWidth); // left bottom left
-	TriLDL(LTL, LL);
-	LTL = Location(Gap+TriWidth, gfx.ScreenHeight - Gap - TriWidth- TriWidth); //left bottom right
-	TriLDL(LTL, LL);
+	LBL = Location(Gap+TriWidth, gfx.ScreenHeight - Gap - TriWidth- TriWidth); //left bottom right
+
+	TriLDL(LBL, LR);
+	RectTopLeft = LTL + Location(0, TriWidth);
+	RectBottomLeft = LBL + Location(TriWidth, 0);
+	Rect(RectTopLeft, RectBottomLeft, LR);
 
 }
 
-void Frame::TriLTL(Location& loc, Color c)
+void Frame::TriLTL(Location& loc, Color& c)
 {
 	int SelfControl=0;
 	for (int y=loc.y; y <= loc.y + TriWidth; y++)
@@ -34,7 +45,7 @@ void Frame::TriLTL(Location& loc, Color c)
 	}
 }
 
-void Frame::TriLTL2(Location& loc, Color c)
+void Frame::TriLTL2(Location& loc, Color& c)
 {
 	int SelfControl = 0;
 	for (int y = loc.y; y <= loc.y + TriWidth; y++)
@@ -42,23 +53,27 @@ void Frame::TriLTL2(Location& loc, Color c)
 		SelfControl++;
 		for (int x = loc.x; x <= loc.x + SelfControl; x++)
 		{
-			gfx.PutPixel(y, x, Colors::Blue);
+			gfx.PutPixel(y, x, c);
 		}
-
 	}
 }
 
-void Frame::TriLDL(Location & loc, Color c)
+void Frame::TriLDL(Location & loc, Color& c)
 {
-	int SelfControl = 11;
+	int SelfControl = TriWidth +1;
 	for (int y = loc.y; y <= loc.y + TriWidth; y++)
 	{
 		SelfControl--;
 		for (int x = loc.x; x <= loc.x + SelfControl; x++)
 		{
-			gfx.PutPixel(x, y, Colors::Red);
+			gfx.PutPixel(x, y, c);
 			// gfx.PutPixel(int(Y), int(X), Colors::Blue); MAKES THE OTHER HALF OF THE TRIANGLE c:
 		}
 
 	}
+}
+
+void Frame::Rect(Location & loc0, Location & loc1, Color & c)
+{
+	gfx.DrawRect(loc0.x, loc0.y, loc1.x , loc1.y , c);
 }
--- Full Body Orgasm Snake/Engine/Frame.h
+++ Full Body Orgasm Snake/Engine/Frame.h
@@ -6,13 +6,14 @@ class Frame
 public:
 	Frame(Graphics& gfx);
 	void DrawAlles();
-	void TriLTL(Location& loc, Color c); // top left side triangle Left (touching the left side)
-	void TriLTL2(Location& loc, Color c);
-	void TriLDL(Location& loc, Color c);
+	void TriLTL(Location& loc, Color& c); // top left side triangle Left (touching the left side)
+	void TriLTL2(Location& loc, Color& c);
+	void TriLDL(Location& loc, Color& c);
+	void Rect(Location& loc, Location & loc1, Color& c);
 private:
 	static constexpr int Gap = 40; // lit.: space from left screen to right of the border 
-	static constexpr int Width = 20;
-	static constexpr int TriWidth = 10;
+	static constexpr int Width = 14;
+	static constexpr int TriWidth = Width /2;
 	Color c = {211,211,211};
 	Color LL = { 160, 160, 160 }; //left left
 	Color LR = { 96 ,96 ,96 }; //left right
